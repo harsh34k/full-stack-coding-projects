21173e974e23b1d76367e4ed5f12c94d5bfa504e
Harsh Kumar <harsh34k@gmail.com>
2025-10-21 16:38:16 +0530

    added prisma

--- event-Manager-app/backend/index.js
+++ event-Manager-app/backend/index.js
@@ -1,7 +1,14 @@
 import express from "express";
-
+import cors from "cors"
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/try", async (req, res) => {
+    //logic->Display a list of all events,( sorted by date (upcoming first).)-> frontend
+    // fetch all the value from db and send it
 
 
+})
 
 app.listen(8000, () => console.log("Server started at Port:8000"))
---
+++ event-Manager-app/backend/prisma/migrations/20251021110654_initiated_with_model_event/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "public"."Event" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "date" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
